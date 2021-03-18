8dca9695a4f92fa0857190fa3bd9b7d4350b4825
Harrold Ventayen <hventayen@csu.fullerton.edu>
2021-03-17 23:56:41 -0700

    add weight framework

--- DumbTheBump/DumbTheBump/AddWeight.swift
+++ DumbTheBump/DumbTheBump/AddWeight.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct Weight {
+    var unit: String
+    var date: String
+    var weight: String
+    
+    func addWeight (unit: String, date: String, weight: String) -> String {
+        var message = "" // Data to be returned for other classes
+        if (unit == "Metric") {
+            message = "\(date), \(weight)kg"
+        }
+        if (unit == "Imperial") {
+            message = "\(date), \(weight)lbs"
+        }
+        return message
+    }
+    
+    
+}
