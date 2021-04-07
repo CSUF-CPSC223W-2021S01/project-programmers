ab13d8754601b79750fea14f6993358c840987fc
Mario Linares <mariolinares@csu.fullerton.edu>
2021-04-06 22:15:44 -0700

    Added a tracker class and made some changes to the weight class

--- DumbTheBump/DumbTheBump/AddWeight.swift
+++ DumbTheBump/DumbTheBump/AddWeight.swift
@@ -7,21 +7,51 @@
 
 import Foundation
 
-struct Weight {
+class Weight {
     var unit: String
-    var date: String
-    var weight: String
+    var month: Int
+    var day: Int
+    var year: Int
+    var weight: Int
     
-    func addWeight (unit: String, date: String, weight: String) -> String {
-        var message = "" // Data to be returned for other classes
-        if (unit == "Metric") {
-            message = "\(date), \(weight)kg"
-        }
-        if (unit == "Imperial") {
-            message = "\(date), \(weight)lbs"
-        }
-        return message
+    init(unit: String, month: Int, day: Int, year: Int, weight: Int) {
+        self.unit = unit
+        self.month = month
+        self.day = day
+        self.year = year
+        self.weight = weight
+    }
+}
+
+class Tracker {
+    var tracker: [Weight] // an array that stores the users progress
+    
+    init() {
+        tracker = []
     }
     
+    // add a weight
+    func addWeight(weight: Weight) {
+        tracker.append(weight)
+    }
     
+
+    // display the weight loss history
+    func history() {
+        print(tracker)
+    }
+}
+
+class User {
+    var name: String
+    var surname: String
+    var prePregnancyWeight: Int // the end goal
+    var currentWeight: Int
+    
+    init(name: String, surname: String, prePregnancyWeight: Int, currentWeight: Int) {
+        self.name = name
+        self.surname = surname
+        self.prePregnancyWeight = prePregnancyWeight
+        self.currentWeight = currentWeight
+    }
 }
