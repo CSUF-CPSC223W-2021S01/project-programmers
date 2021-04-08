b652a71faa4a48cca7e670d4d946fe3e96316f13
harrold <hventayen@csu.fullerton.edu>
2021-04-08 07:52:36 -0700

    weight constraints, weight view controller, attempt of accessing data from view controller to swift file

--- DumbTheBump/DumbTheBump/AddWeight.swift
+++ DumbTheBump/DumbTheBump/AddWeight.swift
@@ -15,11 +15,39 @@ class Weight {
     var weight: Int
     
     init(unit: String, month: Int, day: Int, year: Int, weight: Int) {
-        self.unit = unit
-        self.month = month
+        if unit == "Imperial" || unit == "Metric" {
+            // self.unit = unitTextField?
+            self.unit = unit
+        } else {
+            self.unit = "Error"
+        }
+        if month >= 0 && month <= 12 {
+            // self.month = unitTextField.text?
+            self.month = month
+        } else {
+            self.month = -1
+        }
+        // self.month = monthTextField.text?
         self.day = day
-        self.year = year
-        self.weight = weight
+        if year >= 0 {
+            // self.year = yearTextField.text?
+            self.year = year
+        } else {
+            self.year = -1
+        }
+        if weight >= 0 {
+            // self.weight = weightTextField.text?
+            self.weight = weight
+        } else {
+            self.weight = -1
+        }
+        /*
+         self.unit = unit
+         self.month = month
+         self.day = day
+         self.year = year
+         self.weight = weight
+         */
     }
 }
 
